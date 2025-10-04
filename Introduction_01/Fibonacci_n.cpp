4462f483814193146a060007de7d839db40ecb0b
Ompal Yadav <ompalyadav.dev@gmail.com>
2025-10-04 17:28:59 +0530

    Introduction_01

--- Introduction_01/Fibonacci_n.cpp
+++ Introduction_01/Fibonacci_n.cpp
@@ -1,27 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n, i, last, prev, curr;
-cout << "Enter the number: ";
-cin >> n;
+    cout << "Enter the number: ";
+    cin >> n;
 
-if(n < 0) {
-    cout << "Enter valid number";
-    return 0;
-} else {
-    last = 0;
-    prev = 1;
-    curr = 1;
-}
+    if (n < 0)
+    {
+        cout << "Enter valid number";
+        return 0;
+    }
+    else
+    {
+        last = 0;
+        prev = 1;
+        curr = 1;
+    }
 
-for(i = 2; i <=n; i++) {
-    curr = last + prev;
-    last = prev;
-    prev = curr;
-}
+    for (i = 2; i <= n; i++)
+    {
+        curr = last + prev;
+        last = prev;
+        prev = curr;
+    }
 
-cout << curr;
+    cout << curr;
 
     return 0;
 }
