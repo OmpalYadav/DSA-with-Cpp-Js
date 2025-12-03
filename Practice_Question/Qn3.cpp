3aaf6e0e8e963856096a636c5c5ddae20d016819
Ompal Yadav <ompalyadav.dev@gmail.com>
2025-12-03 16:36:37 +0530

    ComeBack

--- Practice_Question/Qn3.cpp
+++ Practice_Question/Qn3.cpp
@@ -1,34 +1,48 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-int main(){
-    int age;
-    cin>>age;
-
-    if(age>18){
-        cout<<"You Can Vote"<<endl;
-    }else{
-        cout<<"You can not vote"<<endl;
-    } 
-//A number is called an Armstrong number if the sum of cubes of its digits is equal to the number itself. (For 3-digit numbers)  
-int n;
-cin>>n;
-
-int original =n;
-int sum=0;
-
-while(n>0)
+void Age_checking(int age)
 {
-    int digit=n%10;
-    sum+=digit*digit*digit;
-    n=n/10;
+    if (age >= 18)
+    {
+        cout << "Your age is " << age << " so You Can Vote" << endl;
+    }
+    else
+    {
+        cout << "Your age is " << age << " so You Can not Vote" << endl;
+    }
 }
-if(sum==original){
-    cout<<"number "<<sum<<" is armstrong ";
-}else{
-        cout<<"number "<<sum<<" is not armstrong ";
-}
-    return 0;
 
-} 
+void Armstrong_Number(int n)
+{
+    int original = n;
+    int sum = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum += digit * digit * digit;
+        n = n / 10;
+    }
+
+    if (sum == original)
+    {
+        cout << "The " << original << " is Armstrong Number" << endl;
+    }
+    else
+    {
+        cout << "The " << original << " is not Armstrong Number" << endl;
+    }
+}
+
+int main()
+{
+    int age, n;
+    cin >> age>>n;
+    Age_checking(age);
+    Armstrong_Number(n);
+
+    return 0;
+}
+
